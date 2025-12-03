7f5819c9a456535be18773e0fa9f273b266f1f14
Perry Zhu <perryzhu2004@outlook.com>
2025-12-02 22:22:32 -0800

    feat: health check endpoints

--- apps/api/account-service/src/app.controller.ts
+++ apps/api/account-service/src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('health')
+  healthCheck(): { message: string } {
+    return { message: 'CCA-account-service: ok' };
+  }
 }
