8e1e1327686aabb4856f68617a0fe8e888fe4dc0
Perry Zhu <perryzhu2004@outlook.com>
2025-12-19 03:46:41 +0800

    feat: account service logger

--- apps/api/account-service/src/app.service.ts
+++ apps/api/account-service/src/app.service.ts
@@ -1,14 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { testSchema } from '@repo/schema/schema';
 
 @Injectable()
 export class AppService {
   constructor(private readonly configService: ConfigService) {}
+  private readonly logger = new Logger(AppService.name, { timestamp: true });
 
   getHello(): string {
     const auth0Domain = this.configService.get<string>('AUTH0_DOMAIN');
-    console.log('Auth0 Domain from env:', auth0Domain);
+    this.logger.log('Auth0 Domain from env:', auth0Domain);
     testSchema.parse({ num: 123, str: 'Hello Zod' });
     return 'Hello World!';
   }
--- apps/api/account-service/src/main.ts
+++ apps/api/account-service/src/main.ts
@@ -2,11 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
+import { ConsoleLogger, Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: new ConsoleLogger({
+      json: true,
+      colors: true,
+    }),
+  });
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('port') || 6666;
+  const logger = new Logger('Bootstrap', { timestamp: true });
+  const port = configService.get<number>('PORT') || 6666;
+  logger.log(`Starting Account Service on port ${port}...`);
   await app.listen(port);
 }
 
