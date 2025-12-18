9dcdbb9454daa56242f873069e43ca5b9ab6a7a5
Perry Zhu <perryzhu2004@outlook.com>
2025-12-19 01:50:56 +0800

    feat: account service env var

--- apps/api/account-service/src/app.service.ts
+++ apps/api/account-service/src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {}
+
   getHello(): string {
+    const auth0Domain = this.configService.get<string>('AUTH0_DOMAIN');
+    console.log('Auth0 Domain from env:', auth0Domain);
     return 'Hello World!';
   }
 }
--- apps/api/account-service/src/main.ts
+++ apps/api/account-service/src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '@/app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 6666);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('port') || 6666;
+  await app.listen(port);
 }
 
 bootstrap().catch((error) => {
   console.error('Failed to start application:', error);
   process.exit(1);
 });
-
-console.log('Starting app on port', process.env.PORT ?? 6666);
